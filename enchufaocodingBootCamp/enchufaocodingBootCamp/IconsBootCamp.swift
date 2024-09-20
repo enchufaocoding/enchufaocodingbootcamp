4f74bcaf1bb48997767313a2ce2e02eceb406a82
enchufaocoding <enchufaocoding@gmail.com>
2024-09-20 22:28:21 +0200

    IconsBootCamp
    IconsBootCamp

---
+++ enchufaocodingBootCamp/enchufaocodingBootCamp/IconsBootCamp.swift
@@ -0,0 +1,30 @@
+//
+//  IconsBootCamp.swift
+//  enchufaocodingBootCamp
+//
+//  Created by Jose Alberto Rosario Castillo on 20/9/24.
+//
+
+import SwiftUI
+
+struct IconsBootCamp: View {
+    var body: some View {
+       // Image(systemName: "heart")
+        Image(systemName: "person.fill.badge.plus")
+            .renderingMode(.original)
+            .font(.largeTitle)
+          //  .resizable()
+        //    .aspectRatio(contentMode: .fit)
+         //   .scaledToFit()
+          //  .scaledToFill()
+           // .font(.caption)
+         //   .font(.system(size: 200))
+        //    .foregroundStyle(Color(#colorLiteral(red: 0.5807225108, green: 0.066734083, blue: 0, alpha: 1)))
+         //   .clipped()
+          //  .frame(width: 300, height: 200)
+    }
+}
+
+#Preview {
+    IconsBootCamp()
+}
